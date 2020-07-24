3c3b3d237a74f2cc98d5d3138716ba8b03632259
Royce Richmond <miles-militis@hotmail.com>
2020-07-24 15:32:04 -0500

    Add files via upload
    new requirements for the use of opencv and conversion between numpy and PIL Image

--- requirements.sh
+++ requirements.sh
@@ -5,8 +5,9 @@ sudo mv geckodriver /usr/local/bin/
 echo "installed geckodriver in usr/local/bin/"
 rm geckodriver-v0.26.0-linux64.tar.gz
 echo "removing geckodriver files"
-sudo pip install selenium
+sudo pip3 install selenium
 echo "installed selenium"
-sudo pip install -U googlemaps
-echo "installed google maps API"
-
+sudo pip3 install matplotlib
+echo "installed matplotlib"
+sudo pip3 install opencv-python
+echo "installed opencv"
