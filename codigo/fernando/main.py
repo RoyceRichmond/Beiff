6144bf5d56dace968b001c1013948c678425bf4d
Royce Richmond <miles-militis@hotmail.com>
2020-07-27 17:04:16 -0500

    Add files via upload
    main code that return distances in meters, with compensation, legacy with no routine
    maintain for further information

--- codigo/fernando/main.py
+++ codigo/fernando/main.py
@@ -1,13 +1,13 @@
 from opencv import *
 masked=[]
-for a in range(1,4):
+for a in range(1,5):
   masked.append(process_images(str(a))) #appends the 3 masks of the streets      #a=carreteras        #b=avenida principal       #c=calles secundarias
-#for a in range(0,3):
+#for a in range(0,4):
 #  for b in range(0,3):
 #    resultado = contarArea(masked[a][b])
 #    print("pixeles en el area de interes")
 #    imprimirResultado(resultado)
 #    print('\n')
-for a in range(0,3):
+for a in range(0,4):
   print(distancias(masked[a]))
-  print('\n')
+  print('[rapido, medio, disminuido ,lento]')
